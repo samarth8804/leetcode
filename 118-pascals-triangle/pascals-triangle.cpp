c85a670b607395b06d474437411cd8701ea54631
Samarth Agrawal <samarthmittal0808@gmail.com>
2025-09-08 11:34:57 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (89.31%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -5,13 +5,11 @@ public:
         vector<vector<int>> ans;
 
         for(int i=0;i<numRows;i++) {
-            vector<int> arr;
+            vector<int> arr(i+1,1);
             for(int j=0;j<=i;j++) {
-                if(j==0 || j==i) {
-                    arr.push_back(1);
-                }
-                else {
-                    arr.push_back(ans[i-1][j-1] + ans[i-1][j]);
+                
+                if(j!=0 && j!=i) {
+                arr[j] = ans[i-1][j-1] + ans[i-1][j];
                 }
             }
             ans.push_back(arr);
