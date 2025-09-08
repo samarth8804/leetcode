6307880eacee48f981ab28e92d46ce83ee5f69a7
Samarth Agrawal <samarthmittal0808@gmail.com>
2025-09-08 12:21:46 +0530

    Time: 1 ms (58.15%) | Memory: 23.7 MB (47.44%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -2,28 +2,41 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         
-        unordered_map<int,int> mp;
-        vector<int> ans;
-        int n = nums.size();
-        int count = 0;
+        int candidate1 = INT_MIN,candidate2 = INT_MIN,count1 = 0,count2 = 0;
 
-        for(int i=0;i<n;i++) {
-            if(mp.count(nums[i]) && mp[nums[i]] == -1) {
-                continue;
+        for(auto num : nums) {
+            if(candidate1 == num) {
+                count1++;
             }
-            mp[nums[i]]++;
-
-            if(mp[nums[i]] > (n/3)) {
-                ans.push_back(nums[i]);
-                mp[nums[i]] = -1;
-                count++;
+            else if(candidate2 == num) {
+                count2++;
             }
-            if(count==2) {
-                break;
+            else if(count1 == 0) {
+                candidate1 = num;
+                count1++;
+            }
+            else if(count2 == 0){
+                candidate2 = num;
+                count2++;
+            }
+            else{
+                count1--;
+                count2--;
             }
         }
 
-        
+        count1 = 0;
+        count2 = 0;
+
+        for(auto num : nums) {
+            if(candidate1 == num) count1++;
+            if(candidate2 == num) count2++;
+        }
+
+        vector<int> ans;
+        int n = nums.size();
+        if(count1 > (n/3)) ans.push_back(candidate1);
+        if(count2 > (n/3)) ans.push_back(candidate2);
 
         return ans;
     }
