0f7a23b1bc10d8f08cea3c354da9558c8232a93e
Samarth Agrawal <samarthmittal0808@gmail.com>
2025-11-11 12:59:49 +0530

    Time: 0 ms (100.00%) | Memory: 95.2 MB (62.40%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -2,71 +2,72 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         
-        int m = nums1.size();
-        int n = nums2.size();
+        int n1 = nums1.size();
+        int n2 = nums2.size();
 
-        if(m<=n) {
+        if(n1<=n2) {
 
-            int left = 0,right = m;
-
+            int left = 0,right = n1;
+            
             while(left<=right) {
+                int mid1 = (left+right)/2;
+                int mid2 = ((n1+n2+1)/2) - mid1;
 
-                int cut1 = (left+right)/2;
-                int cut2 = ((m+n+1)/2)-cut1;
-
-                double l1 = (cut1==0 ? INT_MIN : nums1[cut1-1]);
-                double r1 = (cut1==m ? INT_MAX : nums1[cut1]);
-                double l2 = (cut2==0 ? INT_MIN : nums2[cut2-1]);
-                double r2 = (cut2==n ? INT_MAX : nums2[cut2]);
+                double l1 = (mid1 == 0) ? INT_MIN : nums1[mid1-1];
+                double l2 = (mid2 == 0) ? INT_MIN : nums2[mid2-1];
+                double r1 = (mid1 == n1) ? INT_MAX : nums1[mid1];
+                double r2 = (mid2 == n2) ? INT_MAX : nums2[mid2];
 
                 if(l1<=r2 && l2<=r1) {
-                    if((m+n) % 2 == 0) {
-                        return ((max(l1,l2) + min(r1,r2)) / 2.0);
+
+                    if((n1+n2)%2 == 0) {
+
+                        return (max(l1,l2)+min(r1,r2))/2.0;
                     }
-                    else {
+                    else{
                         return max(l1,l2);
                     }
                 }
                 else if(l1>r2) {
-                    right = cut1-1;
+                    right = mid1 - 1;
                 }
                 else {
-                    left = cut1+1;
+                    left = mid1 + 1;
                 }
             }
         }
         else {
-            int left = 0,right = n;
+            int left = 0,right = n2;
 
             while(left<=right) {
 
-                int cut1 = (left+right)/2;
-                int cut2 = ((m+n+1)/2)-cut1;
+                int mid1 = (left+right)/2;
+                int mid2 = ((n1+n2+1)/2)-mid1;
 
-                double l1 = (cut1==0 ? INT_MIN : nums2[cut1-1]);
-                double r1 = (cut1==n ? INT_MAX : nums2[cut1]);
-                double l2 = (cut2==0 ? INT_MIN : nums1[cut2-1]);
-                double r2 = (cut2==m ? INT_MAX : nums1[cut2]);
+                double l1 = (mid1 == 0) ? INT_MIN : nums2[mid1-1];
+                double l2 = (mid2 == 0) ? INT_MIN : nums1[mid2-1];
+                double r1 = (mid1 == n2) ? INT_MAX : nums2[mid1];
+                double r2 = (mid2 == n1) ? INT_MAX : nums1[mid2];
 
                 if(l1<=r2 && l2<=r1) {
-                    if((m+n) % 2 == 0) {
-                        return ((max(l1,l2) + min(r1,r2)) / 2.0);
+
+                    if((n1+n2)%2 == 0) {
+
+                        return (max(l1,l2)+min(r1,r2))/2.0;
                     }
-                    else {
+                    else{
                         return max(l1,l2);
                     }
                 }
                 else if(l1>r2) {
-                    right = cut1-1;
+                    right = mid1 - 1;
                 }
                 else {
-                    left = cut1+1;
+                    left = mid1 + 1;
                 }
+
             }
         }
-        
-
-        
 
         return 0;
     }
