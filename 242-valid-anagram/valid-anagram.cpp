84ad2650fcaffdc1c25c26dc1cc9eff5046fc71b
Samarth Agrawal <samarthmittal0808@gmail.com>
2025-11-27 11:48:16 +0530

    Time: 7 ms (20.52%) | Memory: 10 MB (10.92%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        int len1 = s.length();
+        int len2 = t.length();
+
+        if(len1 != len2) {
+            return false;
+        }
+
+        unordered_map<char,int> mp;
+
+        for(int i=0;i<len1;i++) {
+
+            mp[s[i]]++;
+        }
+
+        for(int i=0;i<len2;i++) {
+
+            if(mp[t[i]] - 1 < 0) {
+                return false;
+            }
+            else {
+                mp[t[i]]--;
+            }
+        }
+
+        return true;
+    }
+};
