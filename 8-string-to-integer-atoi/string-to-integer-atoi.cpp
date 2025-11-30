60a38e130e94d5c85f43cc1efec9cc3cb1f81de7
Samarth Agrawal <samarthmittal0808@gmail.com>
2025-11-30 12:55:24 +0530

    Time: 2 ms (18.27%) | Memory: 9.1 MB (80.86%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,69 +1,35 @@
 class Solution {
 public:
     int myAtoi(string s) {
-
+        
         int n = s.length();
+
+        long long result = 0;
 
         int i = 0;
 
-        while (i < n && s[i] == ' ') {
+        while(i<n && s[i] == ' ') {
             i++;
         }
 
-        char sign = '+';
+        int sign = 1;
 
-        if (i < n) {
-            if (s[i] == '+') {
-                sign = '+';
-                i++;
-            } else if (s[i] == '-') {
-                sign = '-';
-                i++;
-            }
-        }
-
-        while (i < n && s[i] == '0') {
+        if(i<n && (s[i] == '+' || s[i] == '-')) {
+            sign = (s[i] == '-') ? -1 : 1;
             i++;
         }
 
-        int start = i;
-
-        while (i < n && isdigit(s[i])) {
-            i++;
-        }
-
-        int end = i - 1;
-
-        if (start<=end) {
-            string atoi;
-
-            if ((end - start + 1) > 10) {
-
-                atoi = s.substr(start, 11);
-            } else {
-                atoi = s.substr(start, end - start + 1);
-            }
-
-            long long num;
-
-            if (atoi.length() > 0) {
-                num = stoll(atoi);
-            }
-
-            if (sign == '-') {
-                num = 0 - num;
-            }
-
-            if (num >= INT_MAX) {
+        while(i<n && isdigit(s[i])) {
+            result = result * 10 + (s[i]-'0');
+            if(result*sign >= INT_MAX) {
                 return INT_MAX;
-            } else if (num <= INT_MIN) {
-                return INT_MIN;
-            } else {
-                return int(num);
             }
+            else if(result*sign <= INT_MIN) {
+                return INT_MIN;
+            }
+            i++;
         }
-        else {
-            return 0;
-        }
+
+        return int(result*sign);
     }
 };
