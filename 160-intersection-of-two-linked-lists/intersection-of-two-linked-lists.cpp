58a5cd7ef3753eca3b9b58a989d9af6511f4acba
Samarth Agrawal <samarthmittal0808@gmail.com>
2025-12-28 12:52:52 +0530

    Time: 53 ms (35.31%) | Memory: 23.9 MB (31.44%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -12,37 +12,10 @@ public:
         
         ListNode *dummy1 = headA;
         ListNode *dummy2 = headB;
-        int len1 = 0;
-        int len2 = 0;
-
-        while(dummy1 != nullptr || dummy2 != nullptr) {
-            if(dummy1 != nullptr) {
-                len1++;
-                dummy1 = dummy1->next;
-            }
-            if(dummy2 != nullptr) {
-                len2++;
-                dummy2 = dummy2->next;
-            }
-        }
-
-        dummy1 = headA;
-        dummy2 = headB;
-
-        if(len1 <= len2) {
-            for(int i=0;i<(len2-len1);i++) {
-                dummy2 = dummy2->next;
-            }
-        }
-        else {
-            for(int i=0;i<(len1-len2);i++) {
-                dummy1 = dummy1->next;
-            }
-        }
 
         while(dummy1 != dummy2) {
-            dummy1 = dummy1->next;
-            dummy2 = dummy2->next;
+            dummy1 = dummy1 == nullptr ? headB : dummy1->next;
+            dummy2 = dummy2 == nullptr ? headA : dummy2->next;
         }
 
         return dummy1;
